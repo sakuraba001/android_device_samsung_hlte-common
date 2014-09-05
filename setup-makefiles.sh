ccc8563036aff21611d1820c0b7bd11ef2a5838c
sakuraba <sakura.bar.com@gmail.com>
2014-09-05 17:43:49 +0900

    add hltekdi

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -233,7 +233,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter hlte hltespr hltevzw,\$(TARGET_DEVICE)),)
+ifneq (\$(filter hlte hltespr hltevzw hltekdi,\$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := PPPreference
